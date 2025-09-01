e13023580c6ec2d5e57257d9028f5ede5dc95643
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2025-09-01 13:40:32 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	log.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", mux)
+}
